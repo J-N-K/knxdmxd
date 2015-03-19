af4b74c28e28f0fcd0c20b6d76570784469413f3
Jan N. Klug <jan.n.klug@rub.de>
2015-03-19 20:14:43 +0100

    changed config test

--- src/test_config.c
+++ src/test_config.c
@@ -50,33 +50,9 @@ int main() {
     
   if (!fp)
     exit(1);
-
-  while ((len = getline(&buf, &bufsize, fp)) != -1) {
-    nl++;
-    config_strip_comment(buf);
-    linestr = config_trim_string(buf);
-    if (linestr != NULL) {
-      valid = config_split_string(linestr, &leftstr, &rightstr);
-      if (valid) {
-	if (rightstr!=NULL) {
-          printf("Config %s set to %s \n", leftstr, rightstr); 
-          free(leftstr);
-	  free(rightstr);
-        } else {
-          printf("Sectionheader %s \n", leftstr);
-	  free(leftstr);
-        }
-      } else {
-        syslog(LOG_ERR, "error in %s:%d", fname, nl);
-      }
-    }
-    
-  };
-  
-  free(buf);
-  
-  printf("Read %d lines \n", nl);
-  
+ 
+  config_parse_channels(fp);
+ 
   fclose(fp);
 
   return 0;
